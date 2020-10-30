13f4c6cb9525c739c2a5fb4bfc003c311fb03faa
ZFan7 <zfan7@slb.com>
2020-10-30 13:39:16 +0800

    change some

--- main/main.js
+++ main/main.js
@@ -1,113 +1,42 @@
 const console_reporter = require("jasmine/lib/reporters/console_reporter");
 
-module.exports = function main() {
-    // console.log("Debug Info");
-    // return 'Hello World!';
-    //return this.input;
-
-    return printReceipt(input);
+module.exports = function main(inputs) {
+    return printReceipt(inputs);
 };
 
-var input = [{
-        Barcode: 'ITEM000000',
-        Name: 'Coca-Cola',
-        Unit: 'bottle',
-        Price: 3.00
-
-    },
-    {
-        Barcode: 'ITEM000000',
-        Name: 'Coca-Cola',
-        Unit: 'bottle',
-        Price: 3.00
-    },
-    {
-        Barcode: 'ITEM000000',
-        Name: 'Coca-Cola',
-        Unit: 'bottle',
-        Price: 3.00
-    },
-    {
-        Barcode: 'ITEM000000',
-        Name: 'Coca-Cola',
-        Unit: 'bottle',
-        Price: 3.00
-    },
-    {
-        Barcode: 'ITEM000000',
-        Name: 'Coca-Cola',
-        Unit: 'bottle',
-        Price: 3.00
-    },
-    {
-        Barcode: 'ITEM000001',
-        Name: 'Sprite',
-        Unit: 'bottle',
-        Price: 3.00
-    },
-    {
-        Barcode: 'ITEM000001',
-        Name: 'Sprite',
-        Unit: 'bottle',
-        Price: 3.00
-    },
-    {
-        Barcode: 'ITEM000004',
-        Name: 'battery',
-        Unit: 'a',
-        Price: 2.00
-    }
-];
-
 function printReceipt(arr) {
-    var title = '***<store earning no money>Receipt ***';
-    var splitLine = '----------------------';
     // Name: Coca-Cola, Quantity: 5 bottles, Unit price: 3.00 (yuan), Subtotal: 15.00 (yuan)
-    var itemNames = [];
+    var names = [];
     var objects = [];
     var strings = [];
+
     for (let i = 0; i < arr.length; i++) {
-        if (itemNames.includes(arr[i].Name)) {
+        if (names.includes(arr[i].Name)) {
             continue;
         }
-        itemNames.push(arr[i].Name);
+        names.push(arr[i].Name);
         objects.push(arr[i]);
     }
 
-    var itemCounts = getCount(arr, itemNames);
+    var counts = getCount(arr, names);
     var sum = 0;
-    for (let i = 0; i < itemNames.length; i++) {
+    for (let i = 0; i < names.length; i++) {
         let stringLine;
         if (objects[i].Unit != 'a') {
-            stringLine = 'Name: ' + itemNames[i] + ', Quantity: ' + itemCounts[i] + ' ' +
-                objects[i].Unit + 's' + ', Unit price: ' + objects[i].Price.toFixed(2) + ' (yuan), Subtotal: ' +
-                (objects[i].Price * itemCounts[i]).toFixed(2) + ' (yuan)';
-            sum += objects[i].Price * itemCounts[i];
+            item = `Name: ${names[i]}, Quantity: ${counts[i]} ${objects[i].Unit}s, Unit price: ${objects[i].Price.toFixed(2)} (yuan), Subtotal: ${(objects[i].Price * counts[i]).toFixed(2)} (yuan)`;
+            sum += objects[i].Price * counts[i];
         } else {
-            stringLine = 'Name: ' + itemNames[i][0].toUpperCase() + itemNames[i].substr(1)  + ', Quantity: ' + itemCounts[i] +
-                ', Unit price: ' + objects[i].Price.toFixed(2) + ' (yuan), Subtotal: ' +
-                (objects[i].Price * itemCounts[i]).toFixed(2) + ' (yuan)';
-            sum += objects[i].Price * itemCounts[i];
+            item = `Name: ${names[i]}, Quantity: ${counts[i]}, Unit price: ${objects[i].Price.toFixed(2)} (yuan), Subtotal: ${(objects[i].Price * counts[i]).toFixed(2)} (yuan)`;
+            sum += objects[i].Price * counts[i];
         }
 
-        console.log(stringLine);
-        strings.push(stringLine);
+        strings.push(item);
     }
-    console.log(strings);
-    var s;
-    s = '***<store earning no money>Receipt ***\n' + strings.join('\n');
-    s = s + '\n----------------------\n' +
-        'Total: ' + sum.toFixed(2) +' (yuan)\n' +
-        '**********************\n';
-    // console.log(s);
-    return s;
-    // strings.forEach(string => {
-    //     s = s + string + '\n';
-    // });
-    // console.log(title + '\n' + s + splitLine + '\n' + 'Total: '+ sum.toFixed(2) +' (yuan)\n'
-    // + '**********************\n');
-}
 
+    var text = '***<store earning no money>Receipt ***\n' + strings.join('\n');
+    text += '\n----------------------\n' + 'Total: ' + sum.toFixed(2) + ' (yuan)\n' + '**********************\n';
+    return text;
+}
 
 function getCount(arr, itemNames) {
     var count = [];
@@ -119,17 +48,4 @@ function getCount(arr, itemNames) {
         count.push(itemCount);
     });
     return count;
-}
-
-function getPrices(arr, itemNames) {
-    var prices = [];
-    for (let i = 0; i < itemNames.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            if (arr[j].Name == i) {
-                prices.push(arr[j].price);
-                break;
-            }
-        }
-    }
-    return prices;
 }
